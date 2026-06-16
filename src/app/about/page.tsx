596291fb55280d24557f3cd9207645461a09e822
Azountou235 <Azountouleseigneur@gmail.com>
2026-06-16 02:38:52 +0100

    Create AboutPage component with FounderCard

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { FounderCard } from '@/components/FounderCard'
+
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto px-6 py-20">
+      <FounderCard />
+    </div>
+  )
+}
