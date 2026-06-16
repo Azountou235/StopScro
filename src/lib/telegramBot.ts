a50ebd8d8fea07ccc09d10b089165fad67fa78f6
Azountou235 <Azountouleseigneur@gmail.com>
2026-06-16 10:20:31 +0100

    Update telegramBot.ts

--- src/lib/telegramBot.ts
+++ src/lib/telegramBot.ts
@@ -1,3 +1,10 @@
-import '../../bot/handlers/admin'
-import '../../bot/handlers/user'
-import '../../bot/handlers/moderation'
+import TelegramBot from 'node-telegram-bot-api'
+
+const token = process.env.TELEGRAM_BOT_TOKEN!
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!
+
+export const bot = new TelegramBot(token, { webHook: true })
+
+export async function setWebhook() {
+  await bot.setWebHook(`${baseUrl}/api/telegram`)
+}
